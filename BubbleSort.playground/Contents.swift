0f04a9e3d76833b97e038c5b137758565934708d
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-11-22 13:37:27 +0700

    add bubble sort algorithm

--- BubbleSort.playground/Contents.swift
+++ BubbleSort.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var str = "Hello, playground"
+let numbers = [5, 4, 3, 2, 1, 6]
+
+// Worst: O(n^2)
+// ascending
+func sortWithBubbleSort(_ numbers: [Int]) -> [Int] {
+    if numbers.isEmpty {
+        return []
+    }
+    
+    var processedNumbers = numbers
+    for _ in 0..<processedNumbers.count {
+        for j in 0..<processedNumbers.count - 1 {
+            if processedNumbers[j] > processedNumbers[j+1] {
+                let temp = processedNumbers[j]
+                processedNumbers[j] = processedNumbers[j+1]
+                processedNumbers[j+1] = temp
+            }
+        }
+    }
+    return processedNumbers
+}
+
+let sortedNumbers = sortWithBubbleSort(numbers)
+print(sortedNumbers)
