1b495296483ac38594f71d11f6e60c56480f51c3
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-11-24 16:00:54 +0700

    Finish SelectionSort Algorithm

---
+++ SelectionSort.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+let numbers = [6, 1, 9, 5, 13, 6, 99, 19, 58, 0]
+
+func selectionSort(_ numbers: [Int]) -> [Int] {
+    var minIndex = 0
+    var processedNumbers = numbers
+    
+    for i in 0..<processedNumbers.count {
+        minIndex = i
+        for j in (i+1)..<processedNumbers.count {
+            if processedNumbers[minIndex] > processedNumbers[j] {
+                minIndex = j
+            }
+            let temp = processedNumbers[i]
+            processedNumbers[i] = processedNumbers[minIndex]
+            processedNumbers[minIndex] = temp
+            
+            // or
+            // processedNumbers.swapAt(i, minIndex)
+        }
+    }
+    
+    return processedNumbers
+}
+
+
+
+print(selectionSort(numbers))
