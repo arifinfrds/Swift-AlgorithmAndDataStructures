95b62c8a14b8c346168312794a3d4b3929c9a084
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-11 16:48:00 +0700

    Add solution to `moveZeroes`

---
+++ MoveZeroes.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// https://leetcode.com/problems/move-zeroes/description/
+// https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/learn/lecture/12310382#notes
+
+
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+class Solution {
+    func moveZeroes(_ nums: [Int]) -> [Int] {
+        var numbers = nums
+        
+        for _ in 0..<numbers.count {
+            for j in 0..<numbers.count - 1 {
+                if numbers[j] == 0 {
+                    let temp = numbers[j]
+                    numbers[j] = numbers[j+1]
+                    numbers[j+1] = temp
+                }
+            }
+        }
+        return numbers
+    }
+}
+
+print(Solution().moveZeroes([0,1,0,3,12]))
